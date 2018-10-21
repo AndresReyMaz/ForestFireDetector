a08a6352fa9e8660e90ad2ade1389db347f5e9c0
Andrés Reynoso Mazoy <andres.reynoso.mazoy@gmail.com>
2018-10-21 14:04:14 -0500

    Push new kernels

--- partition_images.py
+++ partition_images.py
@@ -10,23 +10,23 @@ def partition_images():
 	
 	def get_image_names():
 		# Gets all of the filenames for images in the image path.
-		return sorted([f for f in listdir('./images')])
+		return sorted([f for f in listdir('./newimages')])
 	
 
 	image_names = get_image_names()
-	print(image_names[0])
+	# print(image_names[0])
 	for image_name in image_names:
-		image = cv2.imread('images/' + image_name)
+		image = cv2.imread('newimages/' + image_name)
 		(height, width) = image.shape[:2]
-		print('the image is', height, 'high and', width, 'wide')
-		kernel_height = height // 7
-		kernel_width = width // 7
+		# print('the image is', height, 'high and', width, 'wide')
+		kernel_height = height // 10
+		kernel_width = width // 10
 
-		for y in range(7):
-			for x in range(7):
+		for y in range(10):
+			for x in range(10):
 				print(output_file_id, "- Extracting image around (", kernel_height*y, ",", kernel_width*x, ")")
 				roi = image[kernel_height*y:kernel_height*(y+1),kernel_width*x:kernel_width*(x+1)]
-				cv2.imwrite('kernels/' + str(output_file_id) + '.jpg', roi)
+				cv2.imwrite('newkernels/' + str(output_file_id) + '.jpg', roi)
 				output_file_id += 1
 
 partition_images()
--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import itertools
 
-with open('clasificacion1.txt') as f:
+with open('stats.txt') as f:
 	lines1 = [line.rstrip('\n') for line in f]
 
-with open('clasificacion1.txt') as f:
+with open('class1.txt') as f:
 	lines2 = [line.rstrip('\n') for line in f]
 
 for (a,b) in zip(lines1, lines2):
